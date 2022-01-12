353bfff0746b512afd7fd6bb3e8c2ea1e7bd2ec1
heastrv <amdregzgsh46@gmail.com>
2022-01-12 18:32:32 +0200

    -3-

--- src/com/sus/Main.java
+++ src/com/sus/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(scanner.nextInt() + scanner.nextInt());
+        System.out.println(scanner.nextInt() - scanner.nextInt());
     }
 }
