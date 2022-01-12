da8a15e5dac2450b94ec1b4014a1710b89715e0d
heastrv <amdregzgsh46@gmail.com>
2022-01-12 18:30:09 +0200

    -2-

--- src/com/sus/Main.java
+++ src/com/sus/Main.java
@@ -1,8 +1,11 @@
 package com.sus;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("110122");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + scanner.nextInt());
     }
 }
